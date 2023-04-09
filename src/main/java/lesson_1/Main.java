92b5b8c8f73772bd1ece947b671a2ecbe0672315
Adalberto777 <albert.tliashev@gmail.com>
2023-04-09 20:38:23 +0600

    binary searching

--- src/main/java/lesson_1/Main.java
+++ src/main/java/lesson_1/Main.java
@@ -3,8 +3,8 @@ package lesson_1;
 public class Main {
     public static void main(String[] args) {
         int [] array = new  int[]{8, 7, 6, 1, 2, 3, 9, 4, 5};
-        for (int i = 0; i < array.length; i++) {
-            System.out.print(array[i] + " ");
+        for (int j : array) {
+            System.out.print(j + " ");
         }
         System.out.println();
         // пузырьковая сортировка O(n^2)
@@ -13,8 +13,8 @@ public class Main {
         //DirectSort.directSort(array);
         // сортировка вставкой O(n^2)
         InsertSort.insertSort(array);
-        for (int i = 0; i < array.length; i++) {
-           System.out.print(array[i] + " ");
+        for (int i : array) {
+            System.out.print(i + " ");
         }
         System.out.println();
         // поиск номера элемента массива O(n)
